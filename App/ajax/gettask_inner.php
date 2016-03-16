0183e29be3b1a49b1f6a76129c92d7445fa68ed8
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-03-16 23:38:06 +0100

    Fixed update issue with posts fetched with gettask_inner.php

--- App/ajax/gettask_inner.php
+++ App/ajax/gettask_inner.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: brolaugh
- * Date: 3/1/16
- * Time: 9:03 PM
- */
+* Created by PhpStorm.
+* User: brolaugh
+* Date: 3/1/16
+* Time: 9:03 PM
+*/
 
 
 chdir("../..");
@@ -14,38 +14,38 @@ include_once 'Helper.php';
 if(!isset($_POST['task'])){
   exit();
 }
-  $s = new \App\database\Select();
-  $task = $s->getTaskWithStatus($_POST['task']);
-  $status_level = $s->getAllStatusLevels();
-  ?>
-    <h3>
-      <span class="pull-left"><?php echo $task->title; ?></span>
+$s = new \App\database\Select();
+$task = $s->getTaskWithStatus($_POST['task']);
+$status_level = $s->getAllStatusLevels();
+?>
+<h3>
+  <span ><?php echo $task->title; ?></span>
+  <div class="btn-group">
+    <div class="btn-toolbar">
       <div class="btn-group">
-        <div class="btn-toolbar">
-          <div class="btn-group">
-            <a href="bootstrap-elements.html" data-target="#" class="btn btn-raised btn-<?php echo $task->style_class;?> dropdown-toggle"
-               data-toggle="dropdown">
-              <?php echo $task->level ;?>
-              <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <?php
-              foreach ($status_level as $tl) {
-                if ($task->level != $tl->plain_text) {
-                  ?>
-                  <li><a
-                      href="<?php echo ROOT . "/App/formhandler/changestatus.php?task=" . $task->id . "&status=" . $tl->id; ?>"><span
-                        class="text-<?php echo $tl->style_class; ?>"><?php echo $tl->plain_text; ?></span></a></li>
-                  <?php
-                }
-              }
-
+        <a href="bootstrap-elements.html" data-target="#" class="btn btn-raised btn-<?php echo $task->style_class;?> dropdown-toggle"
+          data-toggle="dropdown">
+          <?php echo $task->level ;?>
+          <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu">
+          <?php
+          foreach ($status_level as $tl) {
+            if ($task->level != $tl->plain_text) {
               ?>
-            </ul>
-          </div>
-        </div>
+              <li data-toggle="modal" data-target="#statusmodal" onclick="javascript:modalFix(<?php echo $task->id . "," . $tl->id . ",'" . $task->title."'";?>)">
+                <span class="text-<?php echo $tl->style_class; ?>"><?php echo $tl->plain_text; ?></span>
+              </li>
+              <?php
+            }
+          }
+
+          ?>
+        </ul>
       </div>
-    </h3>
-    <p class="text-primary">
-        <?php echo $task->description; ?>
-    </p>
+    </div>
+  </div>
+</h3>
+<p class="text-primary">
+  <?php echo $task->description; ?>
+</p>
